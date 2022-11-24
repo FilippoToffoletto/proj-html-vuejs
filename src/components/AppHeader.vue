c1a858baa0f787002563ae049a7e66c9d9cd81a2
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-24 17:22:29 +0100

    [add]Header raw

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,11 +6,17 @@ export default {
 
 <template>
     <div class="bg-image">
-    
-        <header class="container-fluid">
-            <h2>Dig a little deeper into Damon Vaugh</h2>
-            <H3>CIAO</H3>
-        </header>
+        <div class="container-fluid">
+            <nav>
+                navbar
+            </nav>
+            <div class="jumbo-text">
+                <h1 class="">Damon Vaughn</h1>
+                <h2 class="">Best-selling author and the most influential public intellectual in the western world right now.</h2>
+                <h4>- The New York Times</h4>
+            </div>
+        </div>
+        
     </div>
  
 </template>
@@ -18,6 +24,7 @@ export default {
 
 
 <style lang="scss" scoped>
+@use '../styles/partials/variabiles' as *;
 
 .bg-image{
     max-width: 100%;
@@ -26,6 +33,33 @@ export default {
     position: relative;
     background-position: bottom;
     height: 90vh;
+    color: white;
+    nav{
+        height: 70px;
+        background-color: red;
+    }
+    .container-fluid{
+        max-width: 1600px;
+        .jumbo-text{    
+            margin-top: 15rem;
+            max-width: 710px;
+            h1{
+                padding: 10px 0px;
+                color: $saffron;
+            }
+            h2{
+                font-weight: 600;
+                font-size: 2.5rem;
+                line-height: 4rem;
+                padding: 10px 0px;
+            }
+            h4{
+                padding: 10px 0px;
+                font-weight: 200;
+                color: $suva-grey;
+            }
+        }
+    }
 }
 
 
