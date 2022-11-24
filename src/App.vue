a65ae4b4405b7cd36da25b6e78b217506d915291
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-24 15:17:28 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name:'App',
+  components:{
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+  ciao
+</template>
+
+
+
+<style lang="scss">
+@use './styles/general.scss';
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name:'AppHeader'
+}
+</script>
+
+<template>
+  <h2>Dig a little deeper into Damon Vaugh</h2>
+  <H3>CIAO</H3>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
