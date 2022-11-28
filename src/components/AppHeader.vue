058cc5816b86373fe5f5492e4272c69faffd6934
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-28 17:10:57 +0100

    [add]header buttons

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,8 +17,20 @@ export default {
 
 <template>
     <div class="bg-image">
+        
         <div class="container-fluid">
+            
                 <NavBarItem/>
+                <div class="ft-cards">
+            <div class="ft-card-small"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" class="bi bi-folder-fill text-dark-blue mb-2" viewBox="0 0 16 16">
+                    <path
+                        d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
+                </svg><span class="text-dark-blue">Demos</span></div>
+            <div class="ft-card-small"><span class="d-block text-center text-dark-blue fs-3 fw-bold mb-1"><span
+                        class="smaller align-top">&dollar;</span>39</span>
+                        <span class="text-dark-blue">On sale</span></div>
+        </div>
             <div class="jumbo-text">
                 <h1 class="">Damon Vaughn</h1>
                 <h2 class="">Best-selling author and the most influential public intellectual in the western world right now.</h2>
@@ -72,6 +84,28 @@ export default {
         }
     }
 }
+.ft-cards {
+        position: absolute;
+        right: 0;
+        top: 20%;
+
+        .ft-card-small {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            background-color: white;
+            min-height: 50px;
+            line-height: 1;
+            margin: 0.5rem;
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+            font-size: 0.8rem;
+            &:hover{
+                cursor: pointer;
+            }
+        }
+    }
 
 
 
